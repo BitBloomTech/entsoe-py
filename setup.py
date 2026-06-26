7b88189d35ef4470cbe8de3cdc19419863cefb22
Philip Bradstock <philip@bitbloom.tech>
2026-06-26 14:04:42 +0100

    add test dependencies

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed.
     install_requires=['requests', 'pytz', 'beautifulsoup4>=4.11.1', 'pandas==1.5.3'],
+    extras_require={'test': ['pytest', 'python-dotenv']},
 
     include_package_data=True,
     package_data={"entsoe": ["py.typed"]}
